5188213ea86f35b5a5f55cf7c28787799bfae3c4
NSmolyakov <45562598+NSmolyakov@users.noreply.github.com>
2020-02-29 14:14:04 +0700

    fix Suspence

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {Switch, Route} from "react-router-dom";
 import './App.css';
 import {BrowserRouter as Router } from "react-router-dom";
 import TopNav from './components/TopNav/TopNav';
 import Content from './components/Content/Content'
 import LogIn from './components/Content/LogIn/LogIn'
+import PreLoader from './assets/img/PreLoader/PreLoader.svg'
 
 
 function App() {
   return (
 <Router>
-  <Switch>
-    <Route path='/login' component={LogIn}></Route>
-    <Route path='*'>
-        <div className="App">
-          <TopNav />
-          <Content />
-        </div>
-    </Route>
-  </Switch>
-
+  <Suspense fallback={<div><img src={PreLoader} alt='Загрузка'></img></div>}>
+    <Switch>
+      <Route path='/login' component={LogIn}></Route>
+      <Route path='*'>
+          <div className="App">
+            <TopNav />
+            <Content />
+          </div>
+      </Route>
+    </Switch>
+  </Suspense>
 </Router>
    
   );
