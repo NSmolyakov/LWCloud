bbf6c8188e5b16a96e0aca6f3d1ff78d55160820
NSmolyakov <45562598+NSmolyakov@users.noreply.github.com>
2020-03-25 23:43:07 +0700

    fix Account and Notification bug (SVG is not displayed)

--- src/components/Cards/CardGrids/AccountCardGrid.jsx
+++ src/components/Cards/CardGrids/AccountCardGrid.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Card from '../Card'
 import './CardGrid.css'
+import ListZakazov from '../../../assets/img/Cards/ListZakazov.svg'
 
 function AccountCardGrid(props) {
     return (
         <div className='CardGrid'>
-            <Card name='Пополнить счёт' discription='Пополнить личный счёт аккаунта' link='/indev' />
-            <Card name='Использовать баллы' discription='Купить продукцию корпорации и оплатить баллами' link='/indev' />
-            <Card name='Мои данные' discription='Редактировать личные данные аккаунта' link='/indev' />
+            <Card name='Пополнить счёт' discription='Пополнить личный счёт аккаунта' link='/indev' symbol={ListZakazov} />
+            <Card name='Использовать баллы' discription='Купить продукцию корпорации и оплатить баллами' link='/indev' symbol={ListZakazov} />
+            <Card name='Мои данные' discription='Редактировать личные данные аккаунта' link='/indev' symbol={ListZakazov} />
         </div>
     )
 }
--- src/components/Content/Notifications/Notification/Notification.jsx
+++ src/components/Content/Notifications/Notification/Notification.jsx
@@ -6,9 +6,9 @@ import RegistrasionPartner from '../../../../assets/img/Cards/RegistrasionPartne
 function Notification() {
     return(
         <div className='Notfication'>
-            <img src={Ellipse} className='Ellipse' alt='Ellipse'/>
-            <img src={RegistrasionPartner} className='ListZakazov' alt='ListZakazov' />
-            <p>Дистрибьютор 0008904114 успешно зарегистрирован в системе</p>
+            <img src={Ellipse}  alt='Ellipse'/>
+            <img src={RegistrasionPartner} alt='ListZakazov' />
+            <p>Дистрибьютор <strong>0008904114</strong> успешно зарегистрирован в системе</p>
         </div>
         
     )
